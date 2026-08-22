2961de6d7e4ff9328ae3e9d6e606e528fc80cbb7
Idandb <129291579+Idandb@users.noreply.github.com>
2026-08-23 02:59:52 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   colorScheme: 'light',
-  themeColor: '#f0a500',
+ themeColor: '#ffffff',
 }
 
 export default function RootLayout({
